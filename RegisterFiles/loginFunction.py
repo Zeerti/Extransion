59a450ee751426416d56102600e619fc31153dfa
Fibonacci <tuckerwarlock@lostartmagic.com>
2020-02-07 23:37:33 -0700

    Create loginFunction.py
    Need variables for the login numbers entered on GUI

---
+++ RegisterFiles/loginFunction.py
@@ -0,0 +1,40 @@
+# Function to get the login number
+# Read user input for login as string
+# Store string in list
+# Input will be from GUI
+# Print(userLogin) -- only used for testing purposes
+
+## --reserved for later use--
+# length = len(userLogin) # get total length of array to set list() dynamically
+
+# initializing login string from user
+loginString = input("Enter login: ") # This to be replaced by login commands from GUI
+
+# store login numbers from user in array
+userLogin = []
+for i in loginString:
+    userLogin.append(i)
+print(loginString)
+
+for i in userLogin: # need to get variables from GUI and/or mainFunction() 
+    _value = int(i)
+    if(_value == '1'):
+        # Click 1 on screen
+    if(_value == '2'):
+        # Click 2 on screen
+    if(_value == '3'):
+        # Click 3 on screen
+    if(_value == '4'):
+        # Click 4 on screen
+    if(_value == '5'):
+        # Click 5 on screen
+    if(_value == '6'):
+        # Click 6 on screen
+    if(_value == '7'):
+        # Click 7 on screen
+    if(_value == '8'):
+        # Click 8 on screen
+    if(_value == '9'):
+        # Click 9 on screen
+    if(_value == '0'):
+        # Click 0 on screen
