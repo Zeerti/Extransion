55207ab57f838ce26120b8f83adec0dfbd2c923f
Zeerti <skarrj@gmail.com>
2020-02-24 20:07:44 -0700

    flask restful example for REST api

--- REST API/FlaskTest.py
+++ REST API/FlaskTest.py
@@ -1,17 +1,22 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if(request.method == 'POST'):
+class HelloWorld(Resource):
+    def get(self):
+        return {'about':'Hello World!'}
+    def post(self):
         some_json = request.get_json()
-        return jsonify({'You sent': some_json}), 201
-    else:
-        return jsonify({'about':'Hello World'})
+        return {'you sent': some_json}, 201
 
-@app.route('/multi/<int:num>', methods=['GET'])
-def get_multiply10(num):
-    return jsonify({'result': num*10})
+class Multi(Resource):
+    def get(self, num):
+        return {'result': num*10}
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Multi, '/multi/<int:num>')
 
 if __name__ == '__main__':
     app.run(debug=True)
