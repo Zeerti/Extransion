7ac8c81c0a83bf86b1329b9c706ec702dee3ab4d
Garrett Breeden <garrett_breeden@partech.com>
2020-02-27 13:01:52 -0700

    updated code to work with windows 8

--- RegisterFiles/extransion.py
+++ RegisterFiles/extransion.py
@@ -31,13 +31,13 @@ def main():
     try:
         print('Starting EEL GUI...')
         eel.init('web')
-        eel.start('index.html', mode="chrome")
+        eel.start('index.html', port=8000, mode='edge', cmdline_args=['--start-fullscreen', '--browser-startup-dialog'])
     except EnvironmentError:
         print("\n****************\nFailed auto-launching edge. Attemping to load default browswer\n****************\n\nPlease navigate to localhost:8000/index.html manually if it doesn't automatically open")
         # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
-        if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
-            eel.start('index.html', mode ="False")
-        else:
+        try:
+            eel.start('index.html', port=8000, mode=False)
+        except:
             raise
 
 if __name__ == '__main__':
